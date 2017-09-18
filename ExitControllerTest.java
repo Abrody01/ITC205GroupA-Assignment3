d2d7ea87b14589c333e0041d74c49eb7560dfdc3
Jmz01 <jmz_01@live.com>
2017-09-18 17:07:26 +1000

    completed test method for state transitions (adhoc)

--- ExitControllerTest.java
+++ ExitControllerTest.java
@@ -3,17 +3,21 @@ package bcccp.carpark.exit;
 import bcccp.carpark.CarSensor;
 import bcccp.carpark.Carpark;
 import bcccp.carpark.IGate;
+import bcccp.tickets.adhoc.IAdhocTicket;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 /**
  *
  * @author James Nocevski
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExitControllerTest {
     
             Carpark mockCarpark;
@@ -21,9 +25,11 @@ public class ExitControllerTest {
             CarSensor mockIS;
             CarSensor mockOS;
             IExitUI mockUI;
+            IAdhocTicket mockTicket;
             ExitController mockExit;
     
-      
+    
+            
     @Before
 	public void setUp() {
             mockCarpark = mock(Carpark.class);
@@ -31,24 +37,55 @@ public class ExitControllerTest {
             mockIS = mock(CarSensor.class);
             mockOS = mock(CarSensor.class);
             mockUI = mock(IExitUI.class);
+            mockTicket = mock(IAdhocTicket.class);
             mockExit = new ExitController(mockCarpark, mockGate, mockIS, mockOS, mockUI);
+            
 	}
     
     @Test    
     public void initialisationTest() {
         assertTrue(mockExit instanceof ExitController);
         assertTrue(mockIS instanceof CarSensor);
+        assertTrue(mockOS instanceof CarSensor);
         assertEquals(mockExit.getState(), "IDLE");
     }
     
     @Test
-    public void idleToWaitingTest() {
+    public void stateTransitionTestForAdhoc() {        //IDLE to WAITING, WAITING to REJECTED, WAITING to PROCESSED, PROCESSED to TAKEN, TAKEN to EXITING, EXITING to EXITED
+        assertEquals(mockExit.getState(), "IDLE");  //checks initial state
+        
         when(mockIS.carIsDetected()).thenReturn(true);
-        when(mockIS.getId()).thenReturn("Exit Inside Sensor");
-        assertEquals(mockExit.getState(), "IDLE");
+        when(mockIS.getId()).thenReturn("Exit Inside Sensor");      //sets up conditions necessary for transition from IDLE to WAITING
         mockExit.carEventDetected("Exit Inside Sensor", true);
         assertEquals(mockExit.getState(), "WAITING");
+        
+        when(mockExit.isAdhocTicket("Test")).thenReturn(true);
+        when(mockCarpark.getAdhocTicket("Test")).thenReturn(null);      //sets up conditions necessary for transition from WAITNG to REJECTED
+        mockExit.ticketInserted("A");
+        assertEquals(mockExit.getState(), "REJECTED");
+        
+        mockExit.setState(mockExit.state.valueOf("WAITING"));       //sets state back to WAITING and checks it
+        assertEquals(mockExit.getState(), "WAITING");
+        
+        when(mockCarpark.getAdhocTicket("A")).thenReturn(mockTicket);
+        when(mockTicket.isPaid()).thenReturn(true);                     //sets up conditions necessary for transition from WAITING to PROCESSED
+        mockExit.ticketInserted("A");
+        assertEquals(mockExit.getState(), "PROCESSED");
+        
+        mockExit.ticketTaken();
+        assertEquals(mockExit.getState(), "TAKEN");     //checks transition from PROCESSED to TAKEN
+        
+        when(mockOS.carIsDetected()).thenReturn(true);
+        when(mockOS.getId()).thenReturn("Exit Outside Sensor");      //sets up conditions necessary for transition from TAKEN to EXITING
+        mockExit.carEventDetected("Exit Outside Sensor", true);
+        assertEquals(mockExit.getState(), "EXITING");
+        
+        when(mockIS.carIsDetected()).thenReturn(false);
+        when(mockIS.getId()).thenReturn("Exit Inside Sensor");      //sets up conditions necessary for transition from EXITING to EXITED
+        mockExit.carEventDetected("Exit Inside Sensor", false);
+        assertEquals(mockExit.getState(), "EXITED");
     }
+    
     
     @Test(expected=AssertionError.class)
     public void testNullConstructorParam1() {
